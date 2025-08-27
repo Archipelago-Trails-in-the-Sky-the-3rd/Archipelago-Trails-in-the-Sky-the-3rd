da9a21da6d99604b8d48b55a2fcd2264e1468f55
Nhan Nguyen <Nhan.Nguyen@unb.ca>
2025-08-26 22:14:25 -0400

    Fix win condition

--- worlds/tits_the_3rd/client/client.py
+++ worlds/tits_the_3rd/client/client.py
@@ -509,7 +509,7 @@ class TitsThe3rdContext(CommonContext):
     async def check_location(self, location_id: int):
         if not self.game_interface.should_send_and_recieve_items(self.world_player_identifier):
             return
-        if location_id == get_location_id(LocationName.grancel_castle_queens_bedroom):  # Goal location
+        if location_id == get_location_id(LocationName.chapter2_boss_defeated):  # Goal location
             self.finished_game = True
             await self.send_msgs([{"cmd": "StatusUpdate", "status": ClientStatus.CLIENT_GOAL}])
         self.locations_checked.add(location_id)
